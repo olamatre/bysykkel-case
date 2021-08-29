13427a7aa84abd78fca0b23fdc8593c04784a667
Ola Matre <Ola.Matre@ciber.no>
2021-08-29 16:02:25 +0200

    New layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,63 @@
 import './App.css'
 import { useBikeApi } from './services/BikeApi/BikeApi.service'
 
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import React, { Fragment } from 'react'
+import { AppBar, Avatar, Button, CircularProgress, Container, List, ListItem, ListItemAvatar, ListItemSecondaryAction, ListItemText, Toolbar, Typography } from '@material-ui/core'
 import { DirectionsBike } from '@material-ui/icons'
 
 function App() {
   const { stations, isLoading, error, getStations } = useBikeApi()
 
   return (
-    <AppBar position="relative">
-      <Toolbar>
-        <DirectionsBike />
-        <Typography variant="h6">
-          Oslo Bysykkel
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <Fragment>
+      <AppBar position="relative">
+        <Toolbar>
+          <DirectionsBike />
+          <Typography variant="h6">
+            Oslo Bysykkel
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Container maxWidth="sm">
+          {error != null ? (
+            <Typography>
+              {JSON.stringify(error)}
+            </Typography>
+          ) : (
+            <Fragment>
+              {isLoading ? (
+                <CircularProgress />
+              ) : (
+                <List>
+                  {stations.map((station) => (
+                    <ListItem>
+                      <ListItemAvatar>
+                        {station.numBikesAvailable === 0 ? (
+                          <Avatar>
+                            {station.numBikesAvailable}
+                          </Avatar>
+                        ) : (
+                          <Avatar style={{ backgroundColor: 'blue' }}>
+                            {station.numBikesAvailable}
+                          </Avatar>
+                        )}
+                      </ListItemAvatar>
+                      <ListItemText primary={station.name} secondary={station.address}/>
+                      <ListItemSecondaryAction>
+                        <Button variant="contained" color="primary">
+                          Se mer
+                        </Button>
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                  ))}
+                </List>
+              )}
+            </Fragment>
+          )}
+        </Container>
+      </main>
+    </Fragment>
   );
 }
 
